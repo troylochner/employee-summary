6cd5ebcbb47861eb26b79c7675da23e7c58670ea
troylochner <troy.lochner@gmail.com>
2020-10-13 21:09:51 -0400

    Changed up the sequencing of questions. Now prompting for user type first

--- app.js
+++ app.js
@@ -17,12 +17,23 @@ const render = require("./lib/htmlRenderer");
 
 const team = [];
 
-//ITTERATIVE ID
-var xid = 1001
+//USERID
+var userID = 1001
 
 const addUser = () => {
     return inquirer
         .prompt([{
+                type: "list",
+                message: "What type of user would you like to add?",
+                name: "roleType",
+                choices: [
+                    "Manager",
+                    "Engineer",
+                    "Intern"
+                ],
+                default: "Employee"
+            },
+            {
                 type: "input",
                 message: "Employee Name : ",
                 name: "name",
@@ -34,17 +45,7 @@ const addUser = () => {
                 name: "email",
                 default: "John.Doe@email.com"
             },
-            {
-                type: "list",
-                message: "What is the job role?",
-                name: "roleType",
-                choices: [
-                    "Manager",
-                    "Engineer",
-                    "Intern"
-                ],
-                default: "Employee"
-            },
+
             {
                 type: "input",
                 message: "Office Extension : ",
@@ -83,7 +84,7 @@ const addUser = () => {
                     console.log('Manager');
                     const addManager = new Manager(
                         data.name,
-                        xid,
+                        userID,
                         data.email,
                         data.officeNumber
                     )
@@ -93,7 +94,7 @@ const addUser = () => {
                     console.log('Engineer');
                     const addEngineer = new Engineer(
                         data.name,
-                        xid,
+                        userID,
                         data.email,
                         data.github
                     )
@@ -104,7 +105,7 @@ const addUser = () => {
                     console.log('Intern');
                     const addIntern = new Intern(
                         data.name,
-                        xid,
+                        userID,
                         data.email,
                         data.school
                     )
@@ -114,7 +115,7 @@ const addUser = () => {
             switch (data.addMore) {
                 case "Yes":
                     console.log('Adding another');
-                    xid ++;
+                    userID++;
                     addUser();
                     break;
                 case "No":
@@ -128,37 +129,10 @@ const addUser = () => {
 
         })
 }
-
 const renderHTML = () => {
-    if(!fs.existsSync(OUTPUT_DIR)){
+    if (!fs.existsSync(OUTPUT_DIR)) {
         fs.mkdirSync(OUTPUT_DIR);
     }
-    fs.writeFileSync(outputPath,render(team),"utf-8");
+    fs.writeFileSync(outputPath, render(team), "utf-8");
 }
-
 addUser();
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
